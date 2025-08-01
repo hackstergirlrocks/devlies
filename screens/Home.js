20e8f2fe414294c12868060e52f19e05568eb30c
onlyguts <onlyguts.dev@gmail.com>
2025-08-01 14:55:40 +0200

    [Add] - WaitingPage & HomePage avec les images

---
+++ screens/Home.js
@@ -0,0 +1,130 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import { useState } from 'react';
+
+export default function App({ navigation }) {
+  const [pressPlay, setPressPlay] = useState(false);
+  const [pressProfile, setPressProfile] = useState(false);
+  const [pressSkin, setPressSkin] = useState(false);
+  const [pressInfo, setPressInfo] = useState(false);
+  const [pressShop, setPressShop] = useState(false);
+
+  return (
+    <ImageBackground style={styles.container} source={require('../assets/HomePage/desk-home-page-bigger.png')}>
+
+      <View style={styles.MainTop}>
+
+        <Image style={styles.top} source={require('../assets/btn/star.png')} />
+        <Image style={styles.top} source={require('../assets/btn/ecroue.png')} />
+      </View>
+
+      <View style={styles.mainBtn} >
+
+        {/* PLAY BUTTON */}
+        <TouchableOpacity
+          style={styles.switchPage}
+          activeOpacity={1}
+          onPressIn={() => setPressPlay(true)}
+          onPressOut={() => setPressPlay(false)}
+        >
+          {pressPlay
+            ? <Image style={styles.btn} source={require('../assets/btn/play-btn-down.png')} />
+            : <Image style={styles.btn} source={require('../assets/btn/play-btn.png')} />
+          }
+        </TouchableOpacity>
+
+        {/* PROFILE BUTTON */}
+        <TouchableOpacity
+          style={styles.switchPage}
+          activeOpacity={1}
+          onPressIn={() => setPressProfile(true)}
+          onPressOut={() => setPressProfile(false)}
+        >
+          {pressProfile
+            ? <Image style={styles.btn} source={require('../assets/btn/profile-btn-down.png')} />
+            : <Image style={styles.btn} source={require('../assets/btn/profile-btn.png')} />
+          }
+        </TouchableOpacity>
+
+        {/* SKIN BUTTON */}
+        <TouchableOpacity
+          style={styles.switchPage}
+          activeOpacity={1}
+          onPressIn={() => setPressSkin(true)}
+          onPressOut={() => setPressSkin(false)}
+        >
+          {pressSkin
+            ? <Image style={styles.btn} source={require('../assets/btn/skin-btn-down.png')} />
+            : <Image style={styles.btn} source={require('../assets/btn/skin-btn.png')} />
+          }
+        </TouchableOpacity>
+
+        {/* SHOP BUTTON */}
+        {/* INFO BUTTON */}
+        <TouchableOpacity
+          style={styles.switchPage}
+          activeOpacity={1}
+          onPressIn={() => setPressInfo(true)}
+          onPressOut={() => setPressInfo(false)}
+        >
+          {pressInfo
+            ? <Image style={styles.btn} source={require('../assets/btn/info-btn-down.png')} />
+            : <Image style={styles.btn} source={require('../assets/btn/info-btn.png')} />
+          }
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.switchPage}
+          activeOpacity={1}
+          onPressIn={() => setPressShop(true)}
+          onPressOut={() => setPressShop(false)}
+        >
+          {pressShop
+            ? <Image style={styles.btn} source={require('../assets/btn/shop-btn-down.png')} />
+            : <Image style={styles.btn} source={require('../assets/btn/shop-btn.png')} />
+          }
+        </TouchableOpacity>
+
+
+
+      </View>
+
+      <View style={styles.Mainskin}>
+        <Image style={styles.skin} source={require('../assets/skin/onlyguts.png')} />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+
+  },
+  mainBtn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 25
+  },
+  btn: {
+    width: 150,
+    height: 75,
+  },
+  Mainskin: {
+    alignItems: 'center',
+  },
+  skin: {
+    width: 250,
+    height: 250,
+
+  },
+  top: {
+    width: 55,
+    height: 55,
+  },
+  MainTop: {
+    paddingTop: 40,
+    paddingRight: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  }
+});
---
+++ screens/Waiting.js
@@ -0,0 +1,116 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+
+import { useAudioPlayer } from 'expo-audio';
+
+
+
+export default function App({ navigation }) {
+    const audioSource = require('../assets/Song/lonelytree.mp3');
+    const [press, setPress] = useState(false)
+
+    const [pressSound, setPressSound] = useState(false)
+    const [isPressed, setPressed] = useState(false)
+
+    const player = useAudioPlayer(audioSource);
+
+
+    const SongStart = () => {
+        player.seekTo(0);
+        player.play();
+    }
+
+    useEffect(() => {
+     SongStart()
+    }, [])
+
+
+    const NavigateToLogin = () => {
+        navigation.navigate('Home')
+        setPress(!press)
+    }
+
+    const ChangeSound = () => {
+        setPressSound(!pressSound)
+        setPressed(!isPressed)
+        if (isPressed) {
+            // player.seekTo(0);
+            player.play();
+        } else {
+            // player.seekTo(0);
+            player.pause();
+        }
+    }
+
+    return (
+        <ImageBackground style={styles.container} source={require('../assets/WaitingPage/animation-desk-bigger.gif')}>
+            <View style={styles.sound}>
+                <TouchableOpacity
+                    activeOpacity={1}
+                    onPressIn={() => setPressSound(!pressSound)}
+                    onPressOut={() => ChangeSound()}
+                >
+                    {
+                        isPressed ? (
+                            pressSound ? (
+                                <Image source={require('../assets/btn/sound-off-down.png')} />
+                            ) : (
+                                <Image source={require('../assets/btn/sound-off.png')} />
+                            )
+                        ) : (
+                            pressSound ? (
+                                <Image source={require('../assets/btn/sound-on-red-down.png')} />
+                            ) : (
+                                <Image source={require('../assets/btn/sound-on-red.png')} />
+                            )
+                        )
+                    }
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.logo}>
+                <Image source={require('../assets/WaitingPage/logo-animation-bigger.gif')} />
+            </View>
+
+            <TouchableOpacity
+                style={styles.switchPage}
+                activeOpacity={1}
+                onPressIn={() => setPress(!press)}
+                onPressOut={() => NavigateToLogin()}
+            >
+                {press
+                    ? <Image style={styles.btn} source={require('../assets/btn/start-btn1.png')} />
+                    : <Image style={styles.btn} source={require('../assets/btn/start-btn2.png')} />
+                }
+            </TouchableOpacity>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+
+    switchPage: {
+        justifyContent: 'flex-end',
+        marginBottom: 50,
+        marginLeft: 90,
+        marginRight: 90,
+        alignItems: 'center',
+        // backgroundColor: 'red'
+    },
+
+    logo: {
+        padding: 150,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+
+    sound: {
+        alignItems: 'flex-end',
+        top: 50,
+    },
+
+});
