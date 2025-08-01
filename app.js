a27182b1a36729df1156e83451be4afb0b004eb8
Marie <marie.roche32@orange.fr>
2025-08-01 16:14:40 +0200

    [FIX] - adding cors

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ require('./models/connection');
 
 var app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
