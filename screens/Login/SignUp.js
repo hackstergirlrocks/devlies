446852a22eba8def616d99732180d72587504c63
Marie <marie.roche32@orange.fr>
2025-08-03 15:46:26 +0200

    [ADD] - Inputs for SignUp and SignIn pages

--- screens/Login/SignUp.js
+++ screens/Login/SignUp.js
@@ -1,16 +1,41 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground, TextInput } from 'react-native';
 import { useEffect, useState } from 'react';
+import { useFonts } from 'expo-font'
 
 export default function App({ navigation }) {
+    const [fontsLoaded] = useFonts({
+        'Minecraft': require('../../assets/fonts/Minecraft.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
     return (
         <ImageBackground style={styles.container} source={require('../../assets/WaitingPage/animation-desk-bigger.gif')}>
 
             <TouchableOpacity
                 style={styles.topMain}
-                onPress={() =>  navigation.navigate('PageSign')}
+                onPress={() => navigation.navigate('PageSign')}
                 activeOpacity={1}
             >
                 <Image source={require('../../assets/btn/icone-fleche-retour.png')} />
+                <View style={styles.inputs}>
+                    <ImageBackground style={styles.inputImage} source={require('../../assets/input.png')}>
+                        <TextInput style={[styles.username, { textAlign: 'center' }]} placeholderTextColor="black" placeholder='username'></TextInput>
+                    </ImageBackground>
+
+                    <ImageBackground style={styles.inputImage} source={require('../../assets/input.png')}>
+                        <TextInput style={[styles.email, { textAlign: 'center' }]} placeholderTextColor="black" placeholder='email'></TextInput>
+                    </ImageBackground>
+
+                    <ImageBackground style={styles.inputImage} source={require('../../assets/input.png')}>
+                        <TextInput style={[styles.password, { textAlign: 'center' }]} placeholderTextColor="black" placeholder='password'></TextInput>
+                    </ImageBackground>
+
+                    <ImageBackground style={styles.inputImage} source={require('../../assets/input.png')}>
+                        <TextInput style={[styles.passwordConfirm, { textAlign: 'center' }]} placeholderTextColor="black" placeholder='confirm password'></TextInput>
+                    </ImageBackground>
+                </View>
             </TouchableOpacity>
         </ImageBackground>
     );
@@ -24,5 +49,42 @@ const styles = StyleSheet.create({
     topMain: {
         paddingTop: 50
     },
+    inputs: {
+        //backgroundColor: 'rgba(145, 71, 255, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: '133',
+        gap: 30,
+    },
 
+    username: {
+        //backgroundColor: 'rgba(138, 71, 255, 0.5)',
+        fontFamily: 'Minecraft',
+        fontSize: '20',
+        paddingTop: 25,
+
+    },
+    password: {
+        //backgroundColor: 'rgba(138, 71, 255, 0.5)',
+        fontFamily: 'Minecraft',
+        fontSize: '20',
+        paddingTop: 25,
+    },
+    email: {
+        //backgroundColor: 'rgba(138, 71, 255, 0.5)',
+        fontFamily: 'Minecraft',
+        fontSize: '20',
+        paddingTop: 25,
+    },
+    passwordConfirm: {
+        //backgroundColor: 'rgba(138, 71, 255, 0.5)',
+        fontFamily: 'Minecraft',
+        fontSize: '20',
+        paddingTop: 25,
+    },
+    inputImage: {
+       // backgroundColor: 'rgba(255, 99, 71, 0.5)',
+        width: 320,
+        height: 70,
+    }
 });
