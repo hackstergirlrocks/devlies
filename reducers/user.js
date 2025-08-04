d480dfac8bf5ac3009f27462f760c0a3b8a33eb6
Marie <marie.roche32@orange.fr>
2025-08-04 09:35:26 +0200

    [FIX] redux

---
+++ reducers/user.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: { token: null },
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.value.token = action.payload.token;
+    },
+    logout: (state) => {
+      state.value.token = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import user from '../../devlies-backend/reducers/user'
+import user from '../reducers/user'
 
 const persistConfig = {
     key: 'user',
