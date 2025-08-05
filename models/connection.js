b02e5b81710d9eae70abee1502f29248187fe529
onlyguts <onlyguts.dev@gmail.com>
2025-08-05 09:32:39 +0200

    [Update] - .env

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://onlygutsdev:Ruv4yXT0XQDZ0QvA@cluster0.7xogjra.mongodb.net/dev-lies';
-
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+mongoose.connect(process.env.URL_DB, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
