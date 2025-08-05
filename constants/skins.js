7bb83783a3739f1458fc04234ece0c1979c7fde7
onlyguts <onlyguts.dev@gmail.com>
2025-08-05 09:36:53 +0200

    [Add] - Ajout dun comp skin

---
+++ constants/skins.js
@@ -0,0 +1,25 @@
+const skins = {
+    "ange": require("../assets/Skin/ange-big.png"),
+    "astro": require("../assets/Skin/astro-big.png"),
+    "basic": require("../assets/Skin/basic-big.png"),
+    "cat": require("../assets/Skin/cat-big.png"),
+    "clovis": require("../assets/Skin/clovis-big.png"),
+    "clown": require("../assets/Skin/clown-big.png"),
+    "demon": require("../assets/Skin/demon-big.png"),
+    "flower-girl": require("../assets/Skin/flower-girl-big.png"),
+    "guts": require("../assets/Skin/guts-big.png"),
+    "lunette": require("../assets/Skin/lunette-big.png"),
+    "mafia": require("../assets/Skin/mafia-big.png"),
+    "nosferatu": require("../assets/Skin/nosferatu-big.png"),
+    "peach": require("../assets/Skin/peach-big.png"),
+    "plant": require("../assets/Skin/plant-big.png"),
+    "pokemon": require("../assets/Skin/pokemon-big.png"),
+    "robot": require("../assets/Skin/robot-big.png"),
+    "roi": require("../assets/Skin/roi-big.png"),
+    "wolf": require("../assets/Skin/wolf-big.png"),
+    "emo": require("../assets/Skin/emo-big.png"),
+    // "other.png": require("../assets/Skin/other.png"),
+};
+
+
+export default skins;
--- screens/Home/Skin.js
+++ screens/Home/Skin.js
@@ -3,7 +3,8 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useFonts } from 'expo-font'
-import { login, setSkin } from '../../reducers/user';
+import { login, setSkin } from '../../reducers/user'
+import skins from "../../constants/skins";;
 
 export default function App({ navigation }) {
     const dispatch = useDispatch();
@@ -17,29 +18,7 @@ export default function App({ navigation }) {
     // const token = user.token
 
     // Recup le skin du joueur ici
-    const skins = {
-        "ange": require("../../assets/Skin/ange-big.png"),
-        "astro": require("../../assets/Skin/astro-big.png"),
-        "basic": require("../../assets/Skin/basic-big.png"),
-        "cat": require("../../assets/Skin/cat-big.png"),
-        "clovis": require("../../assets/Skin/clovis-big.png"),
-        "clown": require("../../assets/Skin/clown-big.png"),
-        "demon": require("../../assets/Skin/demon-big.png"),
-        "flower-girl": require("../../assets/Skin/flower-girl-big.png"),
-        "guts": require("../../assets/Skin/guts-big.png"),
-        "lunette": require("../../assets/Skin/lunette-big.png"),
-        "mafia": require("../../assets/Skin/mafia-big.png"),
-        "nosferatu": require("../../assets/Skin/nosferatu-big.png"),
-        "peach": require("../../assets/Skin/peach-big.png"),
-        "plant": require("../../assets/Skin/plant-big.png"),
-        "pokemon": require("../../assets/Skin/pokemon-big.png"),
-        "robot": require("../../assets/Skin/robot-big.png"),
-        "roi": require("../../assets/Skin/roi-big.png"),
-        "wolf": require("../../assets/Skin/wolf-big.png"),
-        "emo": require("../../assets/Skin/emo-big.png"),
-        // "other.png": require("../assets/Skin/other.png"),
-    };
-
+   
     const [skinPlayerBdd, setSkinPlayerBdd] = useState([])
     const [countSkin, setCountSkin] = useState(0);
 
